8a56f1269a45afa51d9f1a20b16ebb28faecfbc5
NethminiDev97 <nethmini.we@gmail.com>
2019-06-28 21:12:43 +0530

    .

--- student_instructor-master/student_instructor-master/public/app.jsx
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import React from 'react';
-import {render} from 'react-dom';
-
-import AppContainer from './AppContainer.jsx';
-import AdminContainer from "./AdminContainer";
-
-render(<AppContainer/>, document.getElementById('app'));
